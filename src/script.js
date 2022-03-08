6c1be90922120ea57a3d68214a275468d3f61fbe
cmotu <cmotu@sandiego.edu>
2022-03-08 15:42:32 -0800

    Made alt responsive as well for icon

--- src/script.js
+++ src/script.js
@@ -41,6 +41,9 @@ function showTemperature(response) {
   let iconElement=document.querySelector("#icon");
   iconElement.setAttribute("src", 
   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute(
+    "alt", response.data.weather[0].description);
+  )
   
 }
 function showWeather(currentLocation) {
